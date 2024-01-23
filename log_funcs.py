f06a307a504d24a1b836619c6b8c5969b0af6aff
Johan <johantokarskij@yahoo.se>
2024-01-23 11:50:12 +0100

    updated write_to_excel logic

--- log_funcs.py
+++ log_funcs.py
@@ -1,6 +1,8 @@
+import csv
+from datetime import datetime
+import os
 import logging
 import openpyxl
-import csv
 from collections import Counter
 from datetime import datetime
 
@@ -10,14 +12,38 @@ PATH_TO_XLSX = './logs/login_history.xlsx'
 
 def log_user_login(username):
     logging.info(f'{username}')
-    write_to_excel()
+    write_to_excel(username)
 
 
-def write_to_excel():
+def write_to_excel(username):
+    if os.path.exists(PATH_TO_XLSX):
+        workbook = openpyxl.load_workbook(PATH_TO_XLSX)
+        sheet = workbook.active
+
+        timestamp = datetime.now().strftime('%Y-%m-%d %H')
+        current_timestamp_username = f'{timestamp} {username}'
+
+        need_to_update_count = False
+        for row_number, row in enumerate(sheet.iter_rows(min_row=2, values_only=True), start=2):
+            excel_row_string = f"{row[0]}-{row[1]:02}-{row[2]:02} {row[3][:2]} {row[4]}"
+
+            if current_timestamp_username == excel_row_string:
+                need_to_update_count = True
+                current_count_cell = sheet.cell(row=row_number, column=6)
+                current_count_cell.value += 1
+                break
+        if not need_to_update_count:
+            date_part, hour_part = timestamp.split(' ')
+            year, month, day = date_part.split('-')
+            row = [int(year), int(month), int(day), f'{int(hour_part)}:00 - {int(hour_part)}:59', username, 1]
+            sheet.append(row)
+        workbook.save(PATH_TO_XLSX)
+    else:
         try:
             workbook = openpyxl.Workbook()
             sheet = workbook.active
-            column_names = ['Year', 'Month', 'Day', 'Hour', 'Username', 'Count']
+            column_names = ['Year', 'Month',
+                            'Day', 'Hour', 'Username', 'Count']
             sheet.append(column_names)
 
             with open(PATH_TO_CSV, 'r', newline='', encoding='utf-8') as file:
@@ -38,5 +64,3 @@ def write_to_excel():
             workbook.save(PATH_TO_XLSX)
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
-
-
